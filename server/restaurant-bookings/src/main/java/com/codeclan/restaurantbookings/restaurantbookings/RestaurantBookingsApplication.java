b3544fc566785806aabcc42532541e56a5712e69
beckyjn <rebeccajoynielsen@gmail.com>
2019-05-31 12:59:00 +0100

    adds spring initalizer stuff

---
+++ server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/RestaurantBookingsApplication.java
@@ -0,0 +1,13 @@
+package com.codeclan.restaurantbookings.restaurantbookings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookingsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantBookingsApplication.class, args);
+	}
+
+}

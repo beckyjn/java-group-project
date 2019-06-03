8b643d28959bc62c0f1bd20a3268bb9e17353ff7
amt1 <atreshansky@googlemail.com>
2019-06-03 12:03:54 +0100

    Added projections to embed bookings in the customer data API

--- server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/repositories/CustomerRepository/CustomerRepository.java
+++ server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/repositories/CustomerRepository/CustomerRepository.java
@@ -1,12 +1,16 @@
 package com.codeclan.restaurantbookings.restaurantbookings.repositories.CustomerRepository;
 
 import com.codeclan.restaurantbookings.restaurantbookings.models.Customer;
+import com.codeclan.restaurantbookings.restaurantbookings.projections.EmbedBookings;
+import com.codeclan.restaurantbookings.restaurantbookings.projections.EmbedRestaurantTables;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
+@RepositoryRestResource(excerptProjection = EmbedBookings.class)
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
     List<Customer> findCustomerByName(String name);

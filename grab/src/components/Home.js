b238106f1cf3f976f2adfc11648b309d914ef1d4
sierratango34 <k.sritawan@gmail.com>
2019-06-03 10:49:30 +0100

    added home.js, about.js

---
+++ grab/src/components/Home.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home = () => {
+  <div>
+    <h1>G.R.A.B</h1>
+  </div>;
+};
+
+export default Home;
--- grab/src/components/Navbar.js
+++ grab/src/components/Navbar.js
@@ -1,1 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+const NavBar = () => {
+  <ul>
+    <li>Home</li>
+    <li>Create New Booking</li>
+    <li>Bookings</li>
+    <li>Customers</li>
+  </ul>;
+};
+
+export default NavBar;

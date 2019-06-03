3fa183becee68bb1cb0d9a0a869c3754fa978ff7
marisol <mtoscanor37@gmail.com>
2019-06-03 18:23:42 +0100

    Added route for frequency of customers, fixed problem

--- server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/controllers/RestaurantTableController.java
+++ server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/controllers/RestaurantTableController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/tables")
+@RequestMapping("/restaurant-tables")
 public class RestaurantTableController {
 
     @Autowired
--- server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/repositories/CustomerRepository/CustomerRepository.java
+++ server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/repositories/CustomerRepository/CustomerRepository.java
@@ -2,7 +2,9 @@ package com.codeclan.restaurantbookings.restaurantbookings.repositories.Customer
 
 import com.codeclan.restaurantbookings.restaurantbookings.models.Customer;
 import com.codeclan.restaurantbookings.restaurantbookings.projections.EmbedBookings;
+import net.bytebuddy.TypeCache;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +17,13 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findCustomerByName(String name);
 
     List<Customer> findCustomerById(Long id);
+
+    @Query(value = "SELECT customers.*, COUNT(bookings.*) AS frequency FROM customers\n" +
+            "INNER JOIN bookings\n" +
+            "ON customers.id = bookings.customer_id\n" +
+            "GROUP BY customers.id\n" +
+            "ORDER BY frequency DESC", nativeQuery = true)
+    List<Customer> findAllByBookings();
+
+//    List<Customer> findAllOrderByCountBookingsDesc();
 }

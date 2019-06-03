fbac1044e5f1641df43d07ff2e389766861cfd50
sierratango34 <k.sritawan@gmail.com>
2019-06-03 15:43:21 +0100

    Monday work, react app, starting to fetch data

--- grab/src/App.js
+++ grab/src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
 import Main from "./components/Main";
+import RestaurantContainer from "./containers/RestaurantContainer";
 
 class App extends Component {
   render() {
-    return <Main />;
+    return (
+      <>
+        <Main />
+        <RestaurantContainer />
+      </>
+    );
   }
 }
 
---
+++ grab/src/containers/RestaurantContainer.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+
+class RestaurantContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookings: null
+    };
+  }
+
+  componentDidMount() {
+    const url = "localhost:8080/bookings";
+
+    fetch(url).then(res => {
+      res.json().then(bookings => {
+        this.setState({ bookings: bookings });
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Restaurant Container</h1>
+      </div>
+    );
+  }
+}
+
+export default RestaurantContainer;

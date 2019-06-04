17c2adc4b2e940ba003af62c357fc82420145929
Charlie Walker <charliewalker2016@outlook.com>
2019-06-04 14:19:02 +0100

    added projections bookings

--- server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/projections/EmbedAllForBookings.java
+++ server/restaurant-bookings/src/main/java/com/codeclan/restaurantbookings/restaurantbookings/projections/EmbedAllForBookings.java
@@ -5,11 +5,16 @@ import com.codeclan.restaurantbookings.restaurantbookings.models.Customer;
 import com.codeclan.restaurantbookings.restaurantbookings.models.RestaurantTable;
 import org.springframework.data.rest.core.config.Projection;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.List;
 
 @Projection(name = "embedAllForBookings", types = Booking.class)
 public interface EmbedAllForBookings {
     public Long getId();
+    public LocalDate getDate();
+    public LocalTime getTime();
+    public int getNumberInParty();
     public Customer getCustomer();
     public List<RestaurantTable> getRestaurantTables();
 

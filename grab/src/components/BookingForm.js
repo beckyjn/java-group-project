21b2a9669724e2669f082ea2cbb2a13934ca1f62
beckyjn <rebeccajoynielsen@gmail.com>
2019-06-05 10:13:12 +0100

    start of form creation

--- grab/src/components/BookingForm.js
+++ grab/src/components/BookingForm.js
@@ -5,13 +5,22 @@ class BookingForm extends Component{
     constructor(props){
         super(props);
         this.state = {
-
+            customerId: '',
+            numberOfGuests: '',
+            tables: [],
+            time: '',
+            date: ''
         }
     }
 
     render () {
         return (
-            <h2>Booking Form</h2>
+            <div class="booking-form">
+                <h2>Booking Form</h2>
+                <form id = "booking-form" onSubmit = {this.handleSubmit}>
+                <label for="customer_id" ></label>
+                </form>
+            </div>
         )
     }
 

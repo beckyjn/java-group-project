27ae44d728a30916e77b8ccec84bd6c88c7b8cea
Charlie Walker <charliewalker2016@outlook.com>
2019-06-05 10:47:52 +0100

    added table to bookings list

--- grab/src/components/BookingList.js
+++ grab/src/components/BookingList.js
@@ -3,24 +3,30 @@ import BookingItem from "./BookingItem"
 
 const BookingList = props => {
 
-const bookingsList = props.bookingsData.map((entry, index) => {
+  const bookingsList = props.bookingsData.map((entry, index) => {
+    return(
+      <BookingItem
+      key = {index}
+      time = {entry.time}
+      name = {entry.customer.name}
+      date = {entry.date}
+      partyNo = {entry.numberInParty}
+      >
+      </BookingItem>
+    )
+  });
+
   return(
-  <BookingItem
-    key = {index}
-    time = {entry.time}
-    name = {entry.customer.name}
-    date = {entry.date}  
-    partyNo = {entry.numberInParty}
-    >
-  </BookingItem>)
-});
-
-return(
-  <div>
-  {bookingsList}
-
-  </div>
-);
+    <table>
+    <tr>
+    <th>Date</th>
+    <th>Name</th>
+    <th>Time</th>
+    <th>Cover</th>
+    </tr>
+    {bookingsList}
+    </table>
+  );
 }
 
 

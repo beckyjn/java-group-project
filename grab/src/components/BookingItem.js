8235e0eda69e4c44f6d76552ff5896c885b9a7f9
sierratango34 <k.sritawan@gmail.com>
2019-06-05 15:05:15 +0100

    button takes user to bookings/id however BUG: does not render booking detail

--- grab/src/components/BookingItem.js
+++ grab/src/components/BookingItem.js
@@ -1,14 +1,19 @@
-import React from "react"
+import React from "react";
 
 const BookingItem = props => {
-  return(
-    <tr>
-    <td>{props.date}</td>
-    <td>{props.name}</td>
-    <td>{props.time}</td>
-    <td>{props.partyNo}</td>
-    </tr>
-  )
-}
+  return (
+    <>
+      <tr>
+        <td>{props.date}</td>
+        <td>{props.name}</td>
+        <td>{props.time}</td>
+        <td>{props.partyNo}</td>
+      </tr>
+      <button>
+        <a href={`http://localhost:3000/bookings/${props.id}`}>More Details</a>
+      </button>
+    </>
+  );
+};
 
 export default BookingItem;
--- grab/src/components/BookingList.js
+++ grab/src/components/BookingList.js
@@ -1,35 +1,31 @@
 import React from "react";
-import BookingItem from "./BookingItem"
+import BookingItem from "./BookingItem";
 
 const BookingList = props => {
-
   const bookingsList = props.bookingsData.map((entry, index) => {
-    return(
+    return (
       <BookingItem
-      key = {index}
-      time = {entry.time}
-      name = {entry.customer.name}
-      date = {entry.date}
-      partyNo = {entry.numberInParty}
-      >
-      </BookingItem>
-    )
+        key={index}
+        time={entry.time}
+        name={entry.customer.name}
+        date={entry.date}
+        partyNo={entry.numberInParty}
+        id={entry.id}
+      />
+    );
   });
 
-  return(
+  return (
     <table>
-    <tr>
-    <th>Date</th>
-    <th>Name</th>
-    <th>Time</th>
-    <th>Cover</th>
-    </tr>
-    {bookingsList}
+      <tr>
+        <th>Date</th>
+        <th>Name</th>
+        <th>Time</th>
+        <th>Cover</th>
+      </tr>
+      {bookingsList}
     </table>
   );
-}
-
-
-
+};
 
 export default BookingList;
